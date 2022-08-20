203a50a7f6959fbc3d81909a88f38b784c6dc551
Çağlar Çavdar <caglar.cavdar@windowslive.com>
2022-08-20 15:21:37 +0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+
+import Constants from "expo-constants";
+import InputAutoComplete from "./InputAutoComplete";
+
+const { width, height } = Dimensions.get("window");
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+  latitude: 40.76711,
+  longitude: -73.979704,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} initialRegion={INITIAL_POSITION} />
+      <View style={styles.searchContainer}>
+       <InputAutoComplete
+       label="Origin"
+       placeholder="Search for Start Position"
+       onPlaceSelected={(details)=>{}}/>
+       <InputAutoComplete
+       label="Destination"
+       placeholder="Search for Destination Position"
+       onPlaceSelected={(details)=>{}}/>
+       
+      </View>
     </View>
   );
 }
@@ -13,8 +37,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  searchContainer: {
+    position: "absolute",
+    width: "90%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 8,
+    borderRadius: 8,
+    top: Constants.statusBarHeight,
+  },
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
   },
 });
---
+++ InputAutoComplete.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+import { GooglePlaceDetail } from "react-native-google-places-autocomplete";
+import { GOOGLE_API_KEY } from "./GlobalConsts";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import Constants from "expo-constants";
+
+type InputAutoCompleteProps = {
+  label: string;
+  placeholder: string;
+  onPlaceSelected: (details: GooglePlaceDetail | null) => void;
+};
+
+function InputAutoComplete({
+  label,
+  placeholder,
+  onPlaceSelected,
+}: InputAutoCompleteProps) {
+  return (
+    <>
+      <Text>{label}</Text>
+      <GooglePlacesAutocomplete
+        styles={{ textInput: styles.input }}
+        placeholder={placeholder || ""}
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+          onPlaceSelected(details);
+        }}
+        query={{
+          key: GOOGLE_API_KEY,
+          language: "en",
+        }}
+      />
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  searchContainer: {
+    position: "absolute",
+    width: "90%",
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    elevation: 4,
+    padding: 8,
+    borderRadius: 8,
+    top: Constants.statusBarHeight,
+  },
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
+  },
+});
+export default InputAutoComplete;
